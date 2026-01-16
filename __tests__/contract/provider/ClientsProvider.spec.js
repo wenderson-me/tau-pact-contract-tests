9fce25d16ffbad83867223c7d7344cdc36e11d96
wendy <wendrson22@gmail.com>
2026-01-16 21:49:14 +0000

    fix: add state handlers to provider verification tests

--- __tests__/contract/provider/ClientsProvider.spec.js
+++ __tests__/contract/provider/ClientsProvider.spec.js
@@ -1,6 +1,6 @@
 const path = require("path")
 const { Verifier } = require("@pact-foundation/pact")
-const { server, importData } = require("../../../src/provider")
+const { server, importData, clientRepository } = require("../../../src/provider")
 
 const PORT = 8083
 const SERVER_URL = `http://localhost:${PORT}`
@@ -9,7 +9,6 @@ const PACT_FILE = path.resolve(process.cwd(), "__tests__/contract/pacts/frontend
 let serverInstance
 
 beforeAll((done) => {
-  importData()
   serverInstance = server.listen(PORT, () => {
     console.log(`Clients Service listening on ${SERVER_URL}`)
     done()
@@ -22,11 +21,24 @@ afterAll((done) => {
 
 describe("Clients Service Verification", () => {
   it("validates the expectations of Client Service", () => {
+    const resetData = () => {
+      clientRepository.clear()
+      importData()
+      return Promise.resolve()
+    }
+
+    const stateHandlers = {
+      "i have a list of clients": resetData,
+      "i have a client for ID": resetData,
+      "i create a new client": resetData
+    }
+
     let opts = {
       provider: "ClientsService",
       logLevel: "INFO",
       providerBaseUrl: SERVER_URL,
       pactUrls: [PACT_FILE],
+      stateHandlers: stateHandlers
     }
     return new Verifier(opts).verifyProvider().then(output => {
       console.log("Pact Verification Complete")
